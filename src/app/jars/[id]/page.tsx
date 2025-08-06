b944eec8a07963109a85aaee024cebcf95786cf3
artur.manuilenko <artur.manuilenko@yalantis.net>
2025-08-06 21:53:27 +0300

    feat: add optional searchParams to page component

--- src/app/jars/[id]/page.tsx
+++ src/app/jars/[id]/page.tsx
@@ -2,7 +2,10 @@ import { fetchMainJarInfo } from "@/lib/hooks";
 import Jar from "@/ui/Jar";
 import { redirect } from "next/navigation";
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page({ params }: { 
+  params: { id: string } 
+  searchParams?: { [key: string]: string | string[] | undefined }
+}) {
   const { id } = await params;
 
   if (!id) return redirect("/");
