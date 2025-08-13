265ab9602ca51d1bfd98a111c8ae8cb5a1038901
artur.manuilenko <artur.manuilenko@yalantis.net>
2025-08-13 09:25:48 +0300

    feat: add metadata for localization in jars page component

--- src/app/jars/page.tsx
+++ src/app/jars/page.tsx
@@ -1,6 +1,13 @@
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 import { COOKIE_KEYS } from "@/lib/constants";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Збір",
+  description:
+    "Tanok – інструмент пітримки зборів, де після кожного внеску оживає танцюючий аватар. Підтримайте улюблених авторів та насолоджуйтеся веселим танцем!",
+};
 
 export default async function Page() {
   const cookieStore = await cookies();
